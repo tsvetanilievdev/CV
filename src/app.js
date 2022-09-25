01a1673e38aaeb1884caecf935429cef18a238f5
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2022-09-25 09:05:37 +0300

    up

--- src/app.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.showcase');
-
-menuToggle.addEventListener('click', (e) => {
-
-    
-    menuToggle.classList.contains('active') ? menuToggle.classList.remove('active') : menuToggle.classList.add('active');
-    showcase.classList.contains('active') ? showcase.classList.remove('active') : showcase.classList.add('active');
-})
