e378d6eab0cd497ff5961f37eb33e195658e88d1
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2022-10-05 20:08:24 +0300

    up

--- script/script.js
+++ script/script.js
@@ -1,10 +1,19 @@
 document.getElementById('close-menu').addEventListener('click', closeMenu);
 document.getElementById('show-menu').addEventListener('click', showMenu);
+document.getElementById('toogle-menu').addEventListener('click', toogleProcedures);
+const links = [...document.querySelectorAll('a.pro')];
 
 function closeMenu(event) {
     document.getElementById('toogle-menu').classList.add('closed');
 }
 function showMenu(event) {
     document.getElementById('toogle-menu').classList.remove('closed');
+}
 
+function toogleProcedures(event) {
+    let clickedLink = event.target
+    if (clickedLink.tagName == 'A' && clickedLink.classList.contains('heading')) {
+        links.forEach(x => x.parentElement.classList.add('none'));
+        links.filter(x => x.classList.contains(clickedLink.id)).forEach(x => x.parentElement.classList.remove('none'))
+    }
 }
