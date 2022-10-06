89b6a0bd65076107f6d15d07d26e087b68e1011a
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2022-10-06 09:00:13 +0300

    adding functionality

--- script/script.js
+++ script/script.js
@@ -4,20 +4,29 @@ document.getElementById('close-menu').addEventListener('click', closeMenu);
 document.getElementById('show-menu').addEventListener('click', showMenu);
 document.getElementById('toogle-menu').addEventListener('click', toogleProcedures);
 const links = [...document.querySelectorAll('a.pro')];
+const headingLinks = [...document.querySelectorAll('a.heading')];
+console.log(headingLinks)
 
+//close menu from 'X' link
 function closeMenu(event) {
     document.getElementById('toogle-menu').classList.add('closed');
 }
+//toogle menu
 function showMenu(event) {
-    document.getElementById('toogle-menu').classList.remove('closed');
+    document.getElementById('toogle-menu').classList.toggle('closed');
 }
 
+//toggle procedures and add seleced class to link 
 function toogleProcedures(event) {
     let clickedLink = event.target;
     if (clickedLink.tagName == 'A' && clickedLink.classList.contains('heading')) {
-        //closed all open links
+        //clear .selected class of for not clicked heading links
+        headingLinks.filter(x => x.id != clickedLink.id).forEach(x => x.classList.remove('selected'));
+        //toogle .selected to the clicked heading link
+        clickedLink.classList.toggle('selected');
+        //closed all open links - add .none
         links.filter(x => !x.classList.contains(clickedLink.id)).forEach(x => x.parentElement.classList.add('none'));
-        //when click on heading link, toogle his sublinks
+        //when click on heading link, toogle(.none) his sublinks
         links.filter(x => x.classList.contains(clickedLink.id)).forEach(x => x.parentElement.classList.toggle('none'))
     }
 }
