13c343fbb6fb0e95706e8040293eeb71382b00cc
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2022-10-06 18:48:25 +0300

    update submenu

--- script/script.js
+++ script/script.js
@@ -3,9 +3,9 @@
 document.getElementById('close-menu').addEventListener('click', closeMenu);
 document.getElementById('show-menu').addEventListener('click', showMenu);
 document.getElementById('toogle-menu').addEventListener('click', toogleProcedures);
-const links = [...document.querySelectorAll('a.pro')];
+const links = [...document.querySelectorAll('ul.pro')];
 const headingLinks = [...document.querySelectorAll('a.heading')];
-console.log(headingLinks)
+console.log(links)
 
 //close menu from 'X' link
 function closeMenu(event) {
@@ -21,12 +21,26 @@ function toogleProcedures(event) {
     let clickedLink = event.target;
     if (clickedLink.tagName == 'A' && clickedLink.classList.contains('heading')) {
         //clear .selected class of for not clicked heading links
-        headingLinks.filter(x => x.id != clickedLink.id).forEach(x => x.classList.remove('selected'));
+        console.log(clickedLink)
+
+
         //toogle .selected to the clicked heading link
         clickedLink.classList.toggle('selected');
+        //toogle current list 
+        clickedLink.nextElementSibling.classList.toggle('show');
+
+        //clear not clicked links
+        // headingLinks.filter(x => x.id != clickedLink.id).forEach(x => x.classList.remove('selected'))
+        //hide all not clicked lists
+        // headingLinks.filter(x => !x.classList.contains(x.id)).forEach(x => x.nextElementSibling.classList.remove('show'));
+        /* //clear .selected class of for not clicked heading links
+        headingLinks.filter(x => x.parentElement.id != clickedLink.id).forEach(x => x.parentElement.classList.remove('selected'));
+        //toogle .selected to the clicked heading link
+        clickedLink.classList.toggle('selected');
+        
         //closed all open links - add .none
         links.filter(x => !x.classList.contains(clickedLink.id)).forEach(x => x.parentElement.classList.add('none'));
         //when click on heading link, toogle(.none) his sublinks
-        links.filter(x => x.classList.contains(clickedLink.id)).forEach(x => x.parentElement.classList.toggle('none'))
+        links.filter(x => x.classList.contains(clickedLink.id)).forEach(x => x.parentElement.classList.toggle('none')) */
     }
 }
